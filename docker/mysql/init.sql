57ffb0f21c82280b04e17cde4e83a72acbcca964
DESKTOP-VTVLS5K\DELL <satheesh.r@myfluiditi.com>
2026-05-01 16:51:07 +0530

    Docker + CI/CD setup

---
+++ docker/mysql/init.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE authService;
+CREATE DATABASE employeeService;
+CREATE DATABASE departmentService;
+CREATE DATABASE attendanceService;
+CREATE DATABASE leaveService;
+CREATE DATABASE payrollService;
