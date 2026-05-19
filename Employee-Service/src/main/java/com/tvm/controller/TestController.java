5b0d076bfabd62c4041522731c8ec8367f516bcd
DESKTOP-VTVLS5K\DELL <satheesh.r@myfluiditi.com>
2026-05-19 10:41:00 +0530

    Added Logger for Employee Service

--- Employee-Service/src/main/java/com/tvm/controller/TestController.java
+++ Employee-Service/src/main/java/com/tvm/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.tvm.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/test")
 public class TestController {
+    private static final Logger log =
+            LoggerFactory.getLogger(TestController.class);
 
     @GetMapping("/welcome")
     public String welcome() {
+		log.info("Employee service welcome endpoint called");
         return "Employee Service Working";
     }
 }
